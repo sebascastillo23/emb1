63519a674594ed3928ef4bcd43d926c87feac60e
sebascastillo23 <81252591+sebascastillo23@users.noreply.github.com>
2021-03-25 18:37:05 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -4,7 +4,7 @@ function LED1_On() {
 	
 	document.getElementById("estado").src="/static/images/led_on.png";
     message = new Paho.MQTT.Message("On");
-    message.destinationName = "jomsk@hotmail.com/IoT1";
+    message.destinationName = "javier230895@gmail.com/test";
     client.send(message);
   
 }
@@ -12,7 +12,7 @@ function LED1_Off(){
 	
 	document.getElementById("estado").src="/static/images/led_off.png";
     message = new Paho.MQTT.Message("off");
-    message.destinationName = "jomsk@hotmail.com/IoT1";
+    message.destinationName = "javier230895@gmail.com/test";
     client.send(message);
 }
 
@@ -27,8 +27,8 @@ function LED1_Off(){
   client.onMessageArrived = onMessageArrived;
   var options = {
    useSSL: false,
-    userName: "jomsk@hotmail.com",
-    password: "Jomsk4all1996",
+    userName: "javier230895@gmail.com",
+    password: "audio123",
     onSuccess:onConnect,
     onFailure:doFail
   }
@@ -41,7 +41,7 @@ function LED1_Off(){
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado!");
 	
-    client.subscribe("jomsk@hotmail.com/IoT");
+    client.subscribe("javier230895@gmail.com/test");
     message = new Paho.MQTT.Message("hola desde la web");
     message.destinationName = "jomsk@hotmail.com/IoT1";
     client.send(message);
