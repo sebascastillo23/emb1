262ef5153637c89000f85f3382b3610455e9e374
sebascastillo23 <81252591+sebascastillo23@users.noreply.github.com>
2021-04-13 12:58:16 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,78 +1,15 @@
-3//https://www.eclipse.org/paho/clients/js/
-
-function LED1_On() {
-	
-	document.getElementById("estado").src="/static/images/prendido.png";
-    message = new Paho.MQTT.Message("On");
-    message.destinationName = "javier230895@gmail.com/test";
-    client.send(message);
-  
-}
-function LED1_Off(){	
-	
-	document.getElementById("estado").src="/static/images/apagado.png";
-    message = new Paho.MQTT.Message("off");
-    message.destinationName = "javier230895@gmail.com/test";
-    client.send(message);
-}
-
-
-// Create a client instance
-  //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
-  
-  client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
-
-  // set callback handlers
-  client.onConnectionLost = onConnectionLost;
-  client.onMessageArrived = onMessageArrived;
-  var options = {
-   useSSL: false,
-    userName: "javier230895@gmail.com",
-    password: "audio123",
-    onSuccess:onConnect,
-    onFailure:doFail
-  }
-
-  // connect the client
-  client.connect(options);
-   
-  // called when the client connects
-  function onConnect() {
-    // Once a connection has been made, make a subscription and send a message.
-    console.log("Conectado!");
-	
-    client.subscribe("javier230895@gmail.com/test2");
-    message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "javier230895@gmail.com/test";
-    client.send(message);
-	
-  }
-
-  function doFail(e){
-    console.log(e);
-	
-  }
-
-  // called when the client loses its connection
-  function onConnectionLost(responseObject) {
-    if (responseObject.errorCode !== 0) {
-      console.log("onConnectionLost:"+responseObject.errorMessage);
-
-    }
-  }
-
-  // called when a message arrives
+// called when a message arrives
   function onMessageArrived(message) {
-  var separador=",";
-  var SValue="0";
+    var le=document.getElementById("Sensor");
+    var l=document.getElementById("Sensor1");
     console.log("onMessageArrived:"+message.payloadString);
-    SValue=message.payloadString.split(separador);
-    document.getElementById("sensor").innerHTML=SValue[0];
-    if(SValue[1]=="1"){
-    document.getElementById("estado").src="/static/images/prendido.png";
-	}else if(SValue[1]=="0"){
-    document.getElementById("estado").src="/static/images/apagado.png";
-	}
-
+    variables=(message.payloadString).split(("/"));
   }
-  
+  function mostrard(){
+    var dat=document.getElementById("info");
+    dat.innerHTML=variables[0];
+  }
+  function mostrar2(){
+    var dat=document.getElementById("info");
+    dat.innerHTML=variables[1];
+  }
