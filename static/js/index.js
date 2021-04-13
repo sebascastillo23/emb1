a863c6bf359fc289dff28c1d65da74fa65cc2d65
sebascastillo23 <81252591+sebascastillo23@users.noreply.github.com>
2021-04-12 22:13:45 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -16,10 +16,17 @@ function LED1_Off(){
     client.send(message);
 }
 
+function BTN_His(){
+	document.getElementById("Mostrar").innerHTML = '<h3>Historial</h3>';
+	for(var i = 0; i < vector.length; i++) {
+    		document.getElementById("Mostrar").innerHTML += '<li>'+vector[i]+'</li>'
+	}
+}
 
 // Create a client instance
   //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
-  
+  var vector = [];
+  contador = 0;
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
 
   // set callback handlers
@@ -65,14 +72,16 @@ function LED1_Off(){
   function onMessageArrived(message) {
   var separador=",";
   var SValue="0";
-    console.log("onMessageArrived:"+message.payloadString);
-    SValue=message.payloadString.split(separador);
+        SValue=message.payloadString.split(separador);
     document.getElementById("sensor").innerHTML=SValue[0];
     if(SValue[1]=="1"){
     document.getElementById("estado").src="/static/images/led_on.png";
 	}else if(SValue[1]=="0"){
     document.getElementById("estado").src="/static/images/led_off.png";
 	}
-
+    vector.push(message.payloadString);
+    separar_historial = message.payloadString.split(' ');
+    estado = separar_historial[1];
+    console.log(estado);
   }
   
