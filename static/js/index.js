11302fd0672f98a7e6e170ed1ca76e5ad736dfaa
sebascastillo23 <81252591+sebascastillo23@users.noreply.github.com>
2021-04-13 13:29:52 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,61 +1,58 @@
-3//https://www.eclipse.org/paho/clients/js/
-
 // Create a client instance
   //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
   
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
-
+  var variables="-",informacion;
   // set callback handlers
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
   var options = {
-   useSSL: false,
+    useSSL: false,
     userName: "javier230895@gmail.com",
     password: "audio123",
     onSuccess:onConnect,
     onFailure:doFail
   }
-
+  
   // connect the client
   client.connect(options);
-   
+    
   // called when the client connects
   function onConnect() {
     // Once a connection has been made, make a subscription and send a message.
-    console.log("Conectado!");
-	
-    client.subscribe("javier230895@gmail.com/test2");
-    message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "javier230895@gmail.com/test";
-    client.send(message);
-	
+    console.log("Conectado...");
+  
+    client.subscribe("javier230895@gmail.com/test");
+  
+  
   }
 
   function doFail(e){
     console.log(e);
 	
-  }
+  } 
 
   // called when the client loses its connection
   function onConnectionLost(responseObject) {
     if (responseObject.errorCode !== 0) {
       console.log("onConnectionLost:"+responseObject.errorMessage);
-
     }
-   
+  }
 
+  // called when a message arrives
   function onMessageArrived(message) {
     var le=document.getElementById("Sensor");
     var l=document.getElementById("Sensor1");
     console.log("onMessageArrived:"+message.payloadString);
-    variables=(message.payloadString).split(("/"));
+    variables=(message.payloadString).split(("-"));
   }
-  function mostrard(){
+  function Sens1(){
     var dat=document.getElementById("info");
+    dat.innerHTML="";
     dat.innerHTML=variables[0];
   }
-  function mostrar2(){
+  function Sens2(){
     var dat=document.getElementById("info");
+    dat.innerHTML="";
     dat.innerHTML=variables[1];
   }
-	  }
